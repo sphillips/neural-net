01a05e4b2cc7be4d0e7dcb983fceb4c4b1f7f688
sphillips <phillips.sb@gmail.com>
2018-05-05 23:28:53 +0200

    splitting out recurrent net into separate file

---
+++ recurrent.js
@@ -0,0 +1,17 @@
+const brain = require('brain.js');
+
+// Part 2:
+// Example using recurrent neural net with string training data
+const net = new brain.recurrent.LSTM();
+
+net.train([
+            { input: 'I feel great about the world!', output: 'happy' },
+            { input: 'The world is a terrible place!', output: 'sad' },
+          ], {
+            iterations: 500,
+            log: true
+          });
+
+const output = net.run('the world is great!');  // 'happy'
+
+console.log("result is:", output);
